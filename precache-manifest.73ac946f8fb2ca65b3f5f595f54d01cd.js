be8fda6ed6c6517f91aa84bc007542a4a8ccf56d
Raphael Gutierrez <urd1_17@yahoo.com>
2020-06-11 23:03:43 +0800

    First commit

---
+++ precache-manifest.73ac946f8fb2ca65b3f5f595f54d01cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39823b15c164c391a0175e88264d48a0",
+    "url": "/react-test2/index.html"
+  },
+  {
+    "revision": "eb761707f32f8736ab0e",
+    "url": "/react-test2/static/css/main.0415733a.chunk.css"
+  },
+  {
+    "revision": "8b70e8d11f2118c983e1",
+    "url": "/react-test2/static/js/2.f80f8337.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test2/static/js/2.f80f8337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb761707f32f8736ab0e",
+    "url": "/react-test2/static/js/main.46365020.chunk.js"
+  },
+  {
+    "revision": "ac80a09fc6351f8ab28a",
+    "url": "/react-test2/static/js/runtime-main.7bd438f7.js"
+  }
+]);
